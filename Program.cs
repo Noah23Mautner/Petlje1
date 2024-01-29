2ce163df6c626e5c1906bfb2d5426f2cb95fc81f
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-29 17:01:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startYear = 1900;
+            int endYear = 2100;
+
+            Console.WriteLine("Prijestupne godine u razdoblju od {0} do {1} su:", startYear, endYear);
+
+            for (int year = startYear; year <= endYear; year++)
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    Console.WriteLine(year);
+                }
+
+            }
+            Console.ReadKey();  
+        }
+    }
+}
